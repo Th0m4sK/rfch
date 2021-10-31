a72c809ae9da50ba01dfcb502dd4a9831c8faadd
Th0m4sK <inbox@cyber42.de>
2021-10-31 14:50:36 +0100

    Fertig1.0.0

--- data/script.js
+++ data/script.js
@@ -14,199 +14,79 @@ function myFunction() {
 }
 
 function getValue(Name,mul) {
-  return document.getElementById(Name).value*mul
-}
-function setValue(Name,val) {
-  document.getElementById(Name).value = val;
+  return document.getElementById(Name).value
 }
 
-
-function setMem() {
-  if (nM < 10 && document.getElementById("SOLL" + (nM).toString()).value > -1) {
-    document.getElementById("NEXT").innerText = "Ⓝ M" + (nM+1).toString()
-    document.getElementById("SOLL" + LastnM.toString()).style.border = "20px solid #22ca2a"
-    LastnM =nM
-    document.getElementById("SOLL" + (nM).toString()).style.border = "20px solid #ffc000"
-    
-  }
-  else {
-    document.getElementById("NEXT").innerText = "Ⓝ M1"
-    document.getElementById("SOLL0").style.border = "20px solid #ffc000"
-    document.getElementById("SOLL" + LastnM.toString()).style.border = "20px solid #22ca2a"
-    LastnM=0
-  }
+function Ein1() {
+  let sendtext = ["EIN"];
+  sendtext.push("60")
+  post(sendtext);
+}
+function Ein2() {
+  let sendtext = ["EIN"];
+  sendtext.push("120")
+  post(sendtext);
+}
+function Ein3() {
+  let sendtext = ["EIN"];
+  sendtext.push("240")
+  post(sendtext);
+}
+function Ein4() {
+  let sendtext = ["EIN"];
+  sendtext.push("360")
+  post(sendtext);
+}
+function AUS() {
+  let sendtext = ["EIN"];
+  sendtext.push("0")
+  post(sendtext);
 }
 
+function save() {
+  console.log("SavePar");
+  let sendtext = ["savM"];
+  var i;
+  for (i = 1; i < 7; i++){
+      sendtext.push(getValue("f"+i.toString(),1))
+}
+ post(sendtext);
 
-setInterval(
-              function () {
+  document.getElementById("SAVEPAR").style.background = "#22ca2a";
+  
+}
+
+function fkt_load() {
                   let xhttp = new XMLHttpRequest();
                 xhttp.onreadystatechange = function () {
                   
                   if (this.readyState == 4 && this.status == 200) {
                     const DATA = this.responseText.split("/");
                     console.log(DATA)
-                    document.getElementById("IST").innerText= DATA[0];
-                    if (DATA[1] === "0") {
-                    document.getElementById("IST").style.border = "20px solid #22ca2a";
-                    }
-                    if (DATA[1] === "9") {
-                      document.getElementById("IST").style.border = "20px solid #ff0000";
-                      document.getElementById("IST").innerText= DATA[0]+"REF";
-                    }
-                    maxBereich = parseFloat(DATA[2]);
-                    nM=parseInt(DATA[3])
-                    setMem();
+                    let z = 0;
+                    document.getElementById("f1").value= DATA[z];
+                    z += 1;
+                    document.getElementById("f2").value = DATA[z];
+                    z += 1;
+                    document.getElementById("f3").value = DATA[z];
+                    z += 1;
+                    document.getElementById("f4").value = DATA[z];
+                    z += 1;
+                    document.getElementById("f5").value=  DATA[z];
+                    z += 1;
+                    document.getElementById("f6").value= parseInt(DATA[z]);
+                   
                   }
-                  if (parseFloat(document.getElementById("IST").innerText) === parseFloat(document.getElementById("SOLL").value)) {
-                    document.getElementById("SOLL").style.border = "20px solid #22ca2a";
-                  }
-                  else {
-                    document.getElementById("SOLL").style.border = "20px solid #ff0000";
-                  }
+                  
 
                 };
-                xhttp.open("GET", "/aktPos", true);
+                xhttp.open("GET", "/GetData", true);
                 xhttp.send();
               
               }
-  , 1000);
-
-  
-
-
-function nextM() {
-  let soll = -1
-  let z = nM
-  while (soll === -1 && z < 10) {
-    if (z === 9) {
-      nM = 0
-      soll = document.getElementById("SOLL" + nM).value
-
-    }
-    else {
-      z += 1;
-      nM += 1;
-    }
-    if (document.getElementById("SOLL" + nM.toString()).value > -1)
-      soll = document.getElementById("SOLL" + nM.toString()).value
-      document.getElementById("SOLL").value=soll
-  }
-  post(["NextM", nM])
-  if (nm === 0) {
-    allGreen();
-  }
-  setMem();
- 
-}  
-
-function checkArbeitsbereich(data) {
-  let Alarm = false;
-  let _ist = parseFloat(document.getElementById("IST").innerText)
-  let msg = ""
-
-  if (data[2] === "rel") {
-    _ist = _ist + data[1]
-  }
-  if (data[2] === "abs") {
-    _ist = data[1];
-  }
-    if (_ist  < 0){
-      msg = "absolute Position < als 0mm";
-    Alarm = true;
-  }
-  if (_ist > maxBereich){
-  msg = "absolute Position > als "+maxBereich.toString()+"mm";
-  Alarm = true;
-}
-
-  if (Alarm) {
-    alert(msg);
-    return false;
-}
-  return true;
-}
-
-function fkt_boarder(ev) {
-  console.log("input");
-  document.getElementById(ev.target.id).style.border = "20px solid #ff0202";
-  document.getElementById("SAVEPAR").style.backgroundColor = "#ff0202";
-  
-}
-function allGreen() {
-  var i;
-  for (i = 0; i < 10; i++){
-    document.getElementById("SOLL"+i.toString()).style.border = "20px solid #22ca2a";
-  }
- 
-}
-function save() {
-  console.log("SavePar");
-  let sendtext = ["savM"];
-  var i;
-  for (i = 0; i < 10; i++){
-      sendtext.push(getValue("SOLL"+i.toString(),1))
-}
- post(sendtext);
-  allGreen();
-  document.getElementById("SAVEPAR").style.background = "#22ca2a";
-  
-}
-
-
-function relMinus() {
     
+
     
-  let sendtext = ["SOLL", 0, "rel"]
-  
-  sendtext[1] = document.getElementById("SOLL").value * -1
-  if (checkArbeitsbereich(sendtext)) {
-    post(sendtext);
-}
-  document.getElementById("IST").style.border = "20px solid #ffff00";
-     
-}
-    function relPlus() {
-    
-    
-      let sendtext = ["SOLL", 0, "rel"]
-    
-        sendtext[1] = document.getElementById("SOLL").value 
-    
-        if (checkArbeitsbereich(sendtext)) {
-          post(sendtext);
-      }
-   document.getElementById("IST").style.border = "20px solid #ffff00";
-   
- }
-
- function runMotor() {
-   console.log("Run Motor", document.getElementById("SOLL").value);
-   
-   let sendtext = ["SOLL",0,"abs"]
- 
-  sendtext [1]= document.getElementById("SOLL").value
-    
-  if (checkArbeitsbereich(sendtext)) {
-    post(sendtext);
-}
-  document.getElementById("IST").style.border = "20px solid #ffff00";
-  
-}
-
-
-
-function stopMotor() {
-  
-  let sendtext = ["STOP",1];
-  
- 
-    post(sendtext);
-
-
-
-  
-}
 
  function post(text) {
   var i;
@@ -219,43 +99,16 @@ function stopMotor() {
      
   console.log(sendtext);
   let xhr = new XMLHttpRequest();
-  xhr.open("POST", "/data", true);
+  xhr.open("POST", "/SendData", true);
   xhr.setRequestHeader("Content-Type", "text/plain");
   xhr.send(sendtext);
 }
 
-function fkt_load() {
-  let xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      const DATA = this.responseText.split("/");
-      var i;
-      for (i = 0; i < 10; i++){
-        setValue("SOLL"+i.toString(), parseFloat(DATA[i]));
-      }
-      
-      document.getElementById("IST").innerText= DATA[10];
-      document.getElementById("SOLL").value= parseFloat (DATA[11]);
-      allGreen();
-       }
-  };
-  xhttp.open("GET", "/savM", true);
-  xhttp.send();
-}
+
 
 
 window.addEventListener("load", fkt_load);
 
-document.getElementById("SOLL0").addEventListener("input", fkt_boarder);
-document.getElementById("SOLL1").addEventListener("input", fkt_boarder);
-document.getElementById("SOLL2").addEventListener("input", fkt_boarder);
-document.getElementById("SOLL3").addEventListener("input", fkt_boarder);
-document.getElementById("SOLL4").addEventListener("input", fkt_boarder);
-document.getElementById("SOLL5").addEventListener("input", fkt_boarder);
-document.getElementById("SOLL6").addEventListener("input", fkt_boarder);
-document.getElementById("SOLL7").addEventListener("input", fkt_boarder);
-document.getElementById("SOLL8").addEventListener("input", fkt_boarder);
-document.getElementById("SOLL9").addEventListener("input", fkt_boarder);
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
+#include "LittleFS.h"
+#include <FileSystem.h>
+#include <SerialDebug.h>
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <secrets.h>
+#include <ESP8266WiFiAP.h>
 #ifdef __AVR__
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
@@ -20,35 +28,248 @@ int mast_oben[1] = {1};
 int boot[3] = {3,5, 7};
 int schrift[1] = {6};
 int ring[1] = {8};
+bool AUS = false;
+unsigned int rxcount = 0;
+unsigned long EIN_ZEIT = 0;
+
+int state = 0;
+String rxdat = "";
+String RXDAT = "";
+String LastIN = "";
+float backlslash = 0.5;
+unsigned long oldtime = 0;
+bool NewData = false;
+FileSystem fileSys(DEBUG);
+SerialDebug DebugMain(DEBUG);
+AsyncWebServer server(80);
+
+IPAddress local_ip(192,168,42,1);
+IPAddress gateway(192,168,42,254);
+IPAddress netmask(255,255,255,0);
+
+
+struct MData{
+  String Auskuck = "#ff0000"; 
+  String Segel = "#0000ff";
+  String Boot = "#ff0000"; 
+  String Schrift = "#ffffff"; 
+  String Ring = "#0000ff";
+  String Helligkeit = "100";
+  
+};
+
+MData md;
+MData mdOLD;
+
+String processor(const String &var)
+{ 
+  return "lerr";
+}
+
+String CheckREF(String normal){
+ 
+   return normal;
+ 
+}
+String CreateTXString(){
+   String message = "";
+       // ["PaSpeed","PaAccer","PaResul","PaMax","PaBacksl"]
+        message += md.Auskuck + "/";
+        message += md.Segel + "/";
+        message += md.Boot + "/";
+        message += md.Schrift + "/";
+        message += md.Ring + "/";
+        message += md.Helligkeit;
+        return message;
+}
+void SERVER()
+{
+  // Route for root / web page
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    DebugMain.print("IndexHTML", "LOAD", DEBUG);
+    
+    request->send(LittleFS ,CheckREF ("/index.html"), String(), false, processor);
+  });
+
+  // Route to load style.css file
+  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request) {
+    DebugMain.print("Style.css", "LOAD", DEBUG);
+    request->send(LittleFS, "/style.css", "text/css");
+  });
+  // Rout to load sript.js file
+  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+    DebugMain.print("script.js", "LOAD", DEBUG);
+    request->send(LittleFS, "/script.js", "text/javascript");
+  });
+  
+  server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request) {
+    DebugMain.print("favicon.ico", "LOAD", DEBUG);
+    request->send(LittleFS, "favicon.ico", "text/plain");
+  });
+  
+  
+
+   
+  
+   server.on("/GetData", HTTP_GET, [](AsyncWebServerRequest *request)
+             {
+               String message = CreateTXString();
+               request->send_P(200, "text/plain", message.c_str());
+             });
+     
+   server.on(
+       "/SendData", HTTP_POST, [](AsyncWebServerRequest *request) {}, NULL,
+       [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
+       { 
+         String val[20];
+             for (size_t i = 0; i < len; i++)
+          {
+            rxdat += char(data[i]);
+          }
+
+          if (index + len == total)
+          {
+            
+            DebugMain.print("POST DATA", rxdat, DEBUG);
+            int z = 0;
+            int s = 0;
+
+            while (true)
+            {
+            int n = rxdat.indexOf('/', s);
+            if (s-1 == rxdat.lastIndexOf('/')){
+            break;
+             }
+
+            
+            String va = rxdat.substring(s, n);
+            DebugMain.print("DATA", va, DEBUG);
+            val[z] = va;
+            z++;
+            s = n + 1;
+
+            }
+            rxdat = "";
+
+            request->send(200);
+
+                         
+            
+          }
+        
+         
+         if (val[0] == "savM")
+            {
+
+           mdOLD = md;
+           String mes = "";
+           int z = 1;
+           md.Auskuck = val[z];
+           mes += val[z] + "/";
+           z++;
+           md.Segel = val[z];
+           mes += val[z] + "/";
+           z++;
+           md.Boot = val[z];
+           mes += val[z] + "/";
+           z++;
+           md.Schrift = val[z];
+           mes += val[z] + "/";
+           z++;
+           md.Ring = val[z];
+           mes += val[z] + "/";
+           z++;
+           md.Helligkeit = val[z];
+           mes += val[z] + "/";
+           z++;
+          
+
+
+           fileSys.WriteNewFile((char *)"paraM", (char *)mes.c_str());
+
+          
+            
+         }
+         else if (val[0] == "EIN"){
+          unsigned long tmp1;
+           
+         tmp1= val[1].toInt();
+         if (tmp1>0){
+tmp1 = tmp1  *60* 1000;
+         EIN_ZEIT =  tmp1;
+         oldtime = millis();
+         AUS = false;
+         }
+         else
+         {
+           AUS = true;
+           EIN_ZEIT = 0;
+         }
+
+         DebugMain.print("EIN", String(EIN_ZEIT), DEBUG);
+         DebugMain.print("MILLIS()", String(oldtime), DEBUG);
+         
+
+         }
+
+         
+           
+              });
+
+   // Start server
+   
+}
+
+
+
+
 
 void setPixelLine(int start, int Anzahl, uint8_t r, uint8_t g, uint8_t b ){
-  Serial.print(start);
-  Serial.print(";");
-  Serial.print(Anzahl);
-  Serial.print(";");
-  Serial.print(r);
-  Serial.print(";");
-  Serial.print(g);
-  Serial.print(";");
-  Serial.println(b);
-
-
-
-  
-  for (int i = start; i < start+Anzahl; i++)
+   for (int i = start; i < start+Anzahl; i++)
   {
     pixels.setPixelColor(i, pixels.Color(r, g, b));
   }
 }
-
-void setColor2Gr(int obj[],int size,uint8_t r,uint8_t g,uint8_t b){
+uint8_t getValueHex(String ZAHL) {
+    char Z[5];
+    ZAHL.toCharArray(Z,5);
   
-  for (unsigned long i = 0; i < size ; i++)
+        if (Z[0] >= 48 & Z[0] <= 57) {
+                 
+            return Z[0] - 48;
+        }
+    if (Z[0] >= 65 & Z[0] <= 70) {
+               return Z[0] - 55; 
+    }
+    return -1;
+
+
+}
+uint8_t HEXString_2_INT (String color1,byte Start) {
+    uint8_t zahl = 0;
+    color1.toUpperCase();
+    zahl = getValueHex(color1.substring(Start, Start+1)) * 16;
+  
+    zahl += getValueHex(color1.substring(Start+1, Start+2));
+   
+ 
+    return zahl;
+}
+
+void setColor2Gr(int obj[],int size,String Farbe){
+
+  int r = HEXString_2_INT(Farbe, 1);
+  int g = HEXString_2_INT(Farbe, 3);
+  int b = HEXString_2_INT(Farbe, 5);
+ 
+
+
+  for ( int i = 0; i < size ; i++)
   {
 
-    Serial.println(size);
-    Serial.println(Gr_Start[obj[i]]);
-    Serial.println(Gr_Anzahl[obj[i]]);
+    //Serial.println(size);
+    //Serial.println(Gr_Start[obj[i]]);
+    //Serial.println(Gr_Anzahl[obj[i]]);
 
     setPixelLine(Gr_Start[obj[i]], Gr_Anzahl[obj[i]], r, g, b);
  }
@@ -63,24 +284,107 @@ void setup() {
   // END of Trinket-specific code.
   Serial.begin(115200);
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  fileSys.mount();
+
+  String TXT = fileSys.ReadFile((char *)"paraM");
+ if (TXT=="notExists"){
+   String mes = CreateTXString();
+   mes += "/";
+   fileSys.WriteNewFile((char *)"paraM", (char *)mes.c_str());
+   TXT = fileSys.ReadFile((char *)"paraM");
+ }
+
+int s = 0;
+String val[20];
+int z = 0;
+
+val[0] = "";
+
+while (true)
+{
+  int n = TXT.indexOf('/', s);
+  if (s-1 == TXT.lastIndexOf('/')){
+    break;
+  }
+
+  z++;
+  String va = TXT.substring(s, n);
+  DebugMain.print("DATA", va, DEBUG);
+  val[z] = va;
+  s = n + 1;
+
+ }
+ 
+ z  = 1;
+ 
+ md.Auskuck = val[z];
+ z++;
+md.Segel = val[z];
+ z++;
+ md.Boot = val[z];
+ z++;
+md.Schrift= val[z];
+ z++;
+ md.Ring = val[z];
+z++;
+ md.Helligkeit = val[z];
+
+
+ // Wifi Config
+   //WiFi.begin(SSID, PASS);
+  WiFi.mode(WIFI_AP);           //Only Access point
+  //Start HOTspot removing password will disable security
+	Serial.print("HotSpt IP:");
+	Serial.print("Setting soft-AP configuration ... ");
+	Serial.println(WiFi.softAPConfig(local_ip, gateway, netmask) ? "Ready" : "Failed!");
+
+
+	Serial.print("Setting soft-AP ... ");
+	Serial.println(	WiFi.softAP(SSID, PASS) ? "Ready" : "Failed!");
+
+	Serial.print("Soft-AP IP address = ");
+
+  // Print ESP32 Local IP Address
+  Serial.println(WiFi.localIP());
+  SERVER();
+  server.begin();
+  oldtime = millis();
+  EIN_ZEIT = 60000;
 }
+
+
+
 
 void loop() {
   // Set all pixel colors to 'off'
-  pixels.clear();
+  
+  if (millis()-oldtime>EIN_ZEIT || AUS==true){
+    AUS = true;
+    EIN_ZEIT = 0;
+    pixels.clear();
+pixels.setBrightness(0);
+pixels.show();
+Serial.println("AUS");
+  }
+  else{
+    pixels.clear();
+    Serial.println("EIN");
+    // The first NeoPixel in a strand is #0, second is 1, all the way up
+    // to the count of pixels minus one.
+    setColor2Gr(mast_oben, 1, md.Auskuck);
+    setColor2Gr(segel, 3, md.Segel);
+    setColor2Gr(boot, 3, md.Boot);
+    setColor2Gr(schrift, 1, md.Schrift);
+    setColor2Gr(ring, 1, md.Ring);
+    pixels.setBrightness(int(md.Helligkeit.toInt()*2.55));
 
-  // The first NeoPixel in a strand is #0, second is 1, all the way up
-  // to the count of pixels minus one.
-  setColor2Gr(mast_oben,1, 255, 0, 0);
-  setColor2Gr(segel,3, 0, 0, 255);
-  setColor2Gr(boot,3, 0, 255, 0);
-  setColor2Gr(schrift,1, 255, 255, 0);
-   setColor2Gr(ring,1, 255, 255, 255);
-  pixels.setBrightness(255);
+    pixels.show(); // Send the updated pixel colors to the hardware.
+  }
+    
 
-  pixels.show();   // Send the updated pixel colors to the hardware.
+  
 
-    delay(500000);
+    delay(1000);
 
 
 }
